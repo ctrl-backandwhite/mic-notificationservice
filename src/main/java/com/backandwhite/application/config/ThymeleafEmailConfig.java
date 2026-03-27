20a81fc0a4709565c6396499a78d0a1b894d8751
back and white <01backandwhite@gmail.com>
2026-03-27 10:27:50 +0100

    Configuración de classpath de los templetes

--- src/main/java/com/backandwhite/application/config/ThymeleafEmailConfig.java
+++ src/main/java/com/backandwhite/application/config/ThymeleafEmailConfig.java
@@ -3,6 +3,7 @@ package com.backandwhite.application.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
@@ -11,13 +12,22 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 public class ThymeleafEmailConfig {
 
     @Bean
+    MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setFallbackToSystemLocale(false);
+        return messageSource;
+    }
+
+    @Bean
     SpringTemplateEngine templateEngine(MessageSource messageSource) {
         ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
         resolver.setPrefix("templates/");
         resolver.setSuffix(".html");
         resolver.setTemplateMode(TemplateMode.HTML);
         resolver.setCharacterEncoding("UTF-8");
-        resolver.setCacheable(true);
+        resolver.setCacheable(false);
 
         SpringTemplateEngine engine = new SpringTemplateEngine();
         engine.setTemplateResolver(resolver);
